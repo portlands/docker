f5119f15109f20973512b61fce8858802c1bbfd1
Tobias Sasse <tobi@byte23.de>
2017-10-31 13:54:12 +0100

    added VERSION to build script

--- http2/buildAndPush.sh
+++ http2/buildAndPush.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+VERSION="1.13.6"
+
 docker build --rm -t portlands/http2 .
 
+docker tag http2 portlands/http2:$VERSION
 docker tag http2 portlands/http2:latest
+
+docker push portlands/http2:$VERSION
 docker push portlands/http2:latest
