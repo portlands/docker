a870b74323355ba57e649a2eae8d2b07a2ea7383
Tobias N. Sasse <tobias.sasse@de.ibm.com>
2017-11-10 13:54:22 +0100

    added apk upgrade, fixed buildscript

--- openliberty/buildAndPush.sh
+++ openliberty/buildAndPush.sh
@@ -4,8 +4,8 @@ VERSION="17.0.0.3"
 
 docker build --rm -t portlands/openliberty .
 
-docker tag openliberty portlands/openliberty:$VERSION
-docker tag openliberty portlands/openliberty:latest
+docker tag portlands/openliberty portlands/openliberty:$VERSION
+docker tag portlands/openliberty portlands/openliberty:latest
 
 docker push portlands/openliberty:$VERSION
 docker push portlands/openliberty:latest
